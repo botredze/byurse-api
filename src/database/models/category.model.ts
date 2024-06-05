8a8c6d8d9a2abb2e650fc379325d888e12564cf4
botredze <botedze@gmail.com>
2024-06-05 21:35:59 +0600

    add_photo files

--- src/database/models/category.model.ts
+++ src/database/models/category.model.ts
@@ -7,4 +7,10 @@ export class Category extends Model<Category> {
 
   @Column({ type: DataType.STRING, allowNull: false })
   categoryName: string;
+
+  @Column({ type: DataType.INTEGER, allowNull: false })
+  gender: number;
+
+  @Column({ type: DataType.INTEGER, allowNull: false })
+  type_size: number;
 }
